0109d5cffb2e278e3cab1fb815b653ab52ee1589
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-05 20:50:37 -0500

    modified database.js file

--- db/database.js
+++ db/database.js
@@ -1,10 +1,8 @@
-const util = require('util');
+
 const mysql = require('mysql2');
 
 
-const getConnection = async () => {
-  try {
-    const connection = await mysql.createConnection({
+    const connection =  mysql.createConnection({
       host: 'localhost',
       port: 3306,
       user: 'root',
@@ -12,30 +10,16 @@ const getConnection = async () => {
       database: 'sql_employee_tracker',
     });
 
-    console.log('Connected to the MySQL database. Connection details:', {
-      host: connection.config.host,
-      port: connection.config.port,
-      user: connection.config.user,
-      database: connection.config.database,
-    });
+    console.log('Connected to the MySQL database. Connection details:'), 
+      
+      connection.connect(function (err) {
+        if (err) throw err;
+      });
+      
+      const promiseConnection = connection.promise();
+      
+      module.exports = promiseConnection;
 
-    // Promisify the connection query method
-    connection.query = util.promisify(connection.query).bind(connection);
-
-    // Test the connection with a simple query
-    await connection.query('SELECT 1');
-    console.log('MySQL connection test successful.');
-
-    return connection;
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
-};
-
-module.exports = {
-  getConnection: getConnection,
-};
 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,23 @@
+CREATE DATABASE sql_employee_tracker;
+
+USE sql_employee_tracker;
+
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
 
--- CREATE DATABASE sql_employee_tracker;
 
-USE sql_employee_tracker;
 
-CREATE TABLE department(
+
+
+CREATE TABLE department (
     id INT AUTO_INCREMENT  NOT NULL,
     department_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
     id INT  AUTO_INCREMENT NOT NULL,
     title VARCHAR(255) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
@@ -22,7 +26,7 @@ CREATE TABLE role(
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL ,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
